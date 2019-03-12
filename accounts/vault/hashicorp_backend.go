16010bc2bd659fbb70c019eb2d474ed0068f681c
chris-j-h <chris.hounsom@gmail.com>
2019-03-12 13:11:48 +0000

    Create wallets with vault backend and enable subscribing to walletevents

--- accounts/vault/hashicorp_backend.go
+++ accounts/vault/hashicorp_backend.go
@@ -3,38 +3,69 @@ package vault
 import (
 	"github.com/ethereum/go-ethereum/accounts"
 	"github.com/ethereum/go-ethereum/event"
+	"sync"
 )
 
 type hashicorpBackend struct {
+	stateLock sync.RWMutex
 	wallets []accounts.Wallet // A vault wallet contains all account keys stored in that particular vault and accessible with a particular auth token
+	updateFeed event.Feed
+	updateScope event.SubscriptionScope
+	hashicorpConfigs []hashicorpConfig
 }
 
-type hashicorpData struct {
+type hashicorpConfig struct {
 	clientData clientData
 	secrets []secretData
 }
 
-func newHashicorpBackend(hashicorpData []hashicorpData) *hashicorpBackend {
-	wallets := make([]accounts.Wallet, len(hashicorpData))
-	for i, data := range hashicorpData {
-		w := newHashicorpWallet(data.clientData, data.secrets)
-		wallets[i] = w
-	}
+func newHashicorpBackend(hashicorpConfigs []hashicorpConfig) *hashicorpBackend {
+	hb := &hashicorpBackend{hashicorpConfigs: hashicorpConfigs}
+	hb.refreshWallets()
 
-	return &hashicorpBackend{wallets}
+	return hb
 }
 
 func (hb *hashicorpBackend) Wallets() []accounts.Wallet {
-	hb.refreshWallets()
+	// check connection to vault is still up before returning wallet
+	// update list of accounts in wallets to cover the instances where secrets have been updated/deleted
+	//hb.refreshWallets()
 	return hb.wallets
+}
+
+func (hb *hashicorpBackend) Subscribe(sink chan<- accounts.WalletEvent) event.Subscription {
+	// We need the mutex to reliably start/stop the update loop
+	hb.stateLock.Lock()
+	defer hb.stateLock.Unlock()
+
+	// Subscribe the caller and track the subscriber count
+	sub := hb.updateScope.Track(hb.updateFeed.Subscribe(sink))
+
+	//hb.refreshWallets()
+
+	return sub
 }
 
 func (hb *hashicorpBackend) refreshWallets() {
 	// Check which wallets have been added/dropped and raise corresponding events
 	// Update hb.wallets with refreshed wallets
-}
+	var wallets []accounts.Wallet
+	var events []accounts.WalletEvent
 
-func (*hashicorpBackend) Subscribe(sink chan<- accounts.WalletEvent) event.Subscription {
-	panic("implement me")
+	//TODO consider not only fetching the wallets once (i.e. like in other backend impls)
+	if len(hb.wallets) == 0 {
+		for _, hc := range hb.hashicorpConfigs {
+			w := newHashicorpWallet(hc.clientData, hc.secrets)
+			events = append(events, accounts.WalletEvent{w, accounts.WalletArrived})
+			wallets = append(wallets, w)
+		}
+
+		for _, e := range events {
+			hb.updateFeed.Send(e)
+		}
+
+		hb.wallets = wallets
+	}
+
 }
 
